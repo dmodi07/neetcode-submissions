b46e8bc7c978dfc40b1d3f1904de4d9561b740df
Dipen Modi <165514899+dmodi07@users.noreply.github.com>
2026-06-23 23:00:39 -0400

    Add: next-greater-element-i - submission-0

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        lst = []
+        for i in nums1:
+            for j in range(len(nums2)):
+                if i == nums2[j]:
+                    for k in range(j + 1, len(nums2)):
+                        if nums2[k] > nums2[j]:
+                            lst.append(nums2[k])
+                            break
+                    lst.append(-1)
+        return lst
+                        
+
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        lst = []
+        for i in nums1:
+            for j in range(len(nums2)):
+                if i == nums2[j]:
+                    next_greater = -1
+                    for k in range(j + 1, len(nums2)):  # look right only
+                        if nums2[k] > nums2[j]:          # first greater, not max
+                            next_greater = nums2[k]
+                            break
+                    lst.append(next_greater)
+        return lst
+
